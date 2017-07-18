25a6cd7496a3be6c1e4d3c4d002f78e403977e2b
Anthony Fung <Ant-f@users.noreply.github.com>
2017-07-18 20:55:48 +0100

    Initial commit

---
+++ src/components/ContentRoot.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default class ContentRoot extends React.Component {
+    render() {
+        return (
+            <div>
+                <span>--- Content Root ---</span>
+            </div>
+        )
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist",
+        publicPath: "/dist"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: "awesome-typescript-loader",
+            options: {
+                useBabel: true,
+                babelOptions: {
+                    "presets" : ["es2015"]
+                }
+            }
+        }, {
+            test: /\.js$/,
+            enforce: "pre",
+            loader: "source-map-loader"
+        }]
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};
