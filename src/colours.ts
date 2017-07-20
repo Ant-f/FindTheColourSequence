8ade895f5cf735e63153db3c2a089e98e271658f
Anthony Fung <Ant-f@users.noreply.github.com>
2017-07-20 21:42:11 +0100

    Adding colours enum

---
+++ src/colours.ts
@@ -0,0 +1,11 @@
+export enum Colours {
+  None = "none",
+  Black = "black",
+  Blue = "blue",
+  Green = "green",
+  Orange = "orange",
+  Purple = "purple",
+  Red = "red",
+  White = "white",
+  Yellow = "yellow"
+}
