40f1348f9501ca5490b235448b98ed151b880043
Anthony Fung <Ant-f@users.noreply.github.com>
2017-08-04 23:57:02 +0100

    Fixing bug in creating random colour sequences

--- src/helpers/target-sequence-generator.ts
+++ src/helpers/target-sequence-generator.ts
@@ -8,11 +8,11 @@ const availableColours = Map<Colour, Colour>(Colour)
 export default function(length: number): List<Colour> {
   let targetSequence = Set<Colour>();
 
-  while (targetSequence.count() < length) {
-    let index = Math.floor((Math.random() * availableColours.count()) + 1);
+  while (targetSequence.size < length) {
+    let index = Math.floor((Math.random() * availableColours.size));
 
     while (targetSequence.contains(availableColours.get(index))) {
-      index = (index + 1) % availableColours.count();
+      index = (index + 1) % availableColours.size;
     }
 
     targetSequence = targetSequence.add(availableColours.get(index));
