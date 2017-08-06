e78d368be1845f8c8060a719847b507e83930e38
Anthony Fung <Ant-f@users.noreply.github.com>
2017-08-06 16:27:12 +0100

    Adding feedback-generator

---
+++ src/helpers/feedback-generator.ts
@@ -0,0 +1,29 @@
+import { List } from "immutable";
+import { Colour } from "../../src/model/colour";
+
+const correctColourAndPosition = Colour.Black;
+const correctColourWrongPosition = Colour.White;
+
+/**
+ * Return an appropriate colour indicating match against target sequence
+ * @param colour Colour from sequence to check
+ * @param index Index of colour from sequence to check
+ * @param targetSequence Target colour sequence to match against
+ */
+const checkColour = (colour: Colour, index: number, targetSequence: List<Colour>): Colour => {
+  if (targetSequence.get(index) === colour) {
+    return correctColourAndPosition;
+  }
+  else if (targetSequence.some((sequenceColour) => colour === sequenceColour)) {
+    return correctColourWrongPosition;
+  }
+  return Colour.None;
+};
+
+export default (sequenceToCheck: List<Colour>, targetSequence: List<Colour>): List<Colour> => {
+  const feedback = sequenceToCheck
+    .map((colour, index) => checkColour(colour, index, targetSequence))
+    .toList();
+
+  return feedback;
+};
---
+++ test/helpers/test-feedback-generator.ts
@@ -0,0 +1,63 @@
+// tslint:disable:only-arrow-functions
+
+import { expect } from "chai";
+import { List } from "immutable";
+import { given } from "mocha-testdata";
+import generateFeedback from "../../src/helpers/feedback-generator";
+import { Colour } from "../../src/model/colour";
+
+interface ITestParameters {
+  attempt: Colour[];
+  expectedBlack: number;
+  expectedWhite: number;
+  target: Colour[];
+}
+
+const test1: ITestParameters = {
+  attempt: [Colour.Purple, Colour.Red, Colour.White, Colour.Yellow],
+  expectedBlack: 0,
+  expectedWhite: 0,
+  target: [Colour.Black, Colour.Blue, Colour.Green, Colour.Orange],
+};
+
+const test2: ITestParameters = {
+  attempt: [Colour.Purple, Colour.Red, Colour.White, Colour.Yellow],
+  expectedBlack: 1,
+  expectedWhite: 0,
+  target: [Colour.Purple, Colour.Blue, Colour.Green, Colour.Orange],
+};
+
+const test3: ITestParameters = {
+  attempt: [Colour.Purple, Colour.Red, Colour.White, Colour.Yellow],
+  expectedBlack: 0,
+  expectedWhite: 1,
+  target: [Colour.Black, Colour.Blue, Colour.Green, Colour.Purple],
+};
+
+describe("Feedback Generator", function() {
+  given(test1, test2, test3).it("returns appropriate feedback", function(testCase: ITestParameters) {
+
+    // Arrange
+
+    const targetSequence = List<Colour>(testCase.attempt);
+    const sequenceToCheck = List<Colour>(testCase.target);
+
+    // Act
+
+    const feedback = generateFeedback(sequenceToCheck, targetSequence);
+
+    // Assert
+
+    const blackCount = feedback.count((c) => c === Colour.Black);
+    const whiteCount = feedback.count((c) => c === Colour.White);
+    const noneCount = feedback.count((c) => c === Colour.None);
+
+    const expectedNone =
+      targetSequence.size - (testCase.expectedBlack + testCase.expectedWhite);
+
+    expect(feedback.size).to.equal(targetSequence.size);
+    expect(blackCount).to.equal(testCase.expectedBlack, "Unexpected Black count");
+    expect(whiteCount).to.equal(testCase.expectedWhite, "Unexpected White count");
+    expect(noneCount).to.equal(expectedNone, "Unexpected None count");
+  });
+});
