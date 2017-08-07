1a2429523f6eef378100b82910010dd7e391d734
Anthony Fung <Ant-f@users.noreply.github.com>
2017-08-07 23:34:33 +0100

    Assigning unique keys to components on render

--- src/components/content-root.tsx
+++ src/components/content-root.tsx
@@ -8,17 +8,20 @@ export default class ContentRoot extends React.Component<ContentRootProps> {
     return (
       <div style={{ display: "inline-grid" }}>
         {
-          this.props.data.map((d, listIndex) =>
-            <div style={{ display: "inline-flex" }}>
+          this.props.data.map((d, inputIndex) =>
+            <div
+              key={`input-${inputIndex}`}
+              style={{ display: "inline-flex" }}>
               <SequenceAttempt
-                attemptId={listIndex}
-                colours={d.input}
-                key={listIndex}>
+                attemptId={inputIndex}
+                colours={d.input}>
               </SequenceAttempt>
 
               {
-                d.feedback.map((feedbackSegment) =>
-                  <div style={{ marginLeft: "0.5em", marginRight: "0.5em" }}>
+                d.feedback.map((feedbackSegment, feedbackIndex) =>
+                  <div
+                    key={`input-${inputIndex}-feedback-${feedbackIndex}`}
+                    style={{ marginLeft: "0.5em", marginRight: "0.5em" }}>
                     { feedbackSegment }
                   </div>,
                 )
