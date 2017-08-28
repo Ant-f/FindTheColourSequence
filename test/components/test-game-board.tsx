f0fad524c2dc52c9fd0d1fdeb90523e6b2fc083d
Anthony Fung <Ant-f@users.noreply.github.com>
2017-08-28 20:41:30 +0100

    Game-over message tests

---
+++ test/components/test-game-board.tsx
@@ -0,0 +1,74 @@
+// tslint:disable:only-arrow-functions
+
+import { expect } from "chai";
+import { shallow } from "enzyme";
+import { List } from "immutable";
+import * as React from "react";
+import BetterLuck from "../../src/components/better-luck-banner";
+import ColourSelect from "../../src/components/colour-select";
+import Congratulations from "../../src/components/congratulations-banner";
+import { Colour } from "../../src/model/colour";
+
+describe("<GameBoard/>", function() {
+  describe("Win Message", function() {
+
+    it("is shown when isGameWon is true", function(done) {
+
+      const gameBoardModule = require("inject-loader!../../src/components/game-board");
+
+      const GameBoardWithInjection = gameBoardModule({
+        "../containers/colour-select-container": {
+          default: () => (
+            <ColourSelect
+              onColourSelected={(c: Colour) => { return; }}
+              availableColours={List()} />),
+        },
+      }).default;
+
+      const wrapper = shallow(
+        <GameBoardWithInjection
+          data={[]}
+          isGameWon={true}
+          isGameLost={false}
+          onNewGamePrompt={() => { return; }}
+          showModal={(e: JSX.Element) => {
+            expect(e.type).to.equal((<Congratulations />).type);
+            done();
+          }} />,
+      );
+
+      wrapper.instance().componentDidUpdate({}, {}, {});
+    });
+  });
+
+  describe("Better Luck Message", function() {
+
+    it("is shown when isGameLost is true", function(done) {
+
+      const gameBoardModule = require("inject-loader!../../src/components/game-board");
+
+      const GameBoardWithInjection = gameBoardModule({
+        "../containers/colour-select-container": {
+          default: () => (
+            <ColourSelect
+              onColourSelected={(c: Colour) => { return; }}
+              availableColours={List()} />),
+        },
+      }).default;
+
+      const wrapper = shallow(
+        <GameBoardWithInjection
+          data={[]}
+          isGameWon={false}
+          isGameLost={true}
+          onNewGamePrompt={() => { return; }}
+          showModal={(e: JSX.Element) => {
+            expect(e.type).to.equal((<BetterLuck />).type);
+            done();
+          }} />,
+      );
+
+      wrapper.instance().componentDidUpdate({}, {}, {});
+    });
+  });
+});
