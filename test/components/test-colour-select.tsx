c6489b0a3e7d8c487f293899fe0a6af9414dce35
Anthony Fung <Ant-f@users.noreply.github.com>
2017-08-29 20:19:16 +0100

    Tidy up test-colour-select

--- test/components/test-colour-select.tsx
+++ test/components/test-colour-select.tsx
@@ -1,7 +1,7 @@
 // tslint:disable:only-arrow-functions
 
 import { expect } from "chai";
-import { shallow } from "enzyme";
+import { shallow, ShallowWrapper } from "enzyme";
 import { List } from "immutable";
 import { given } from "mocha-testdata";
 import * as React from "react";
@@ -9,44 +9,30 @@ import ColourSelect from "../../src/components/colour-select";
 import { Colour } from "../../src/model/colour";
 import * as buttons from "../../stylesheets/sass/buttons.scss";
 
+const wrapColourSelect = (isGameOver: boolean): ShallowWrapper => {
+  const wrapper = shallow(
+    <ColourSelect
+      onColourSelected={(c: Colour) => { return; }}
+      availableColours={List([Colour.Blue, Colour.Red])}
+      isGameOver={isGameOver} />);
+
+  return wrapper;
+};
+
 describe("<ColourSelect/>", function() {
-  describe("disabled", function() {
-    it("is true if game is over", function() {
+  describe("colour input", function() {
+    given(true, false).it("is disabled if game is over", function(isGameOver: boolean) {
 
-      const wrapper = shallow(
-        <ColourSelect
-          onColourSelected={(c: Colour) => { return; }}
-          availableColours={List([Colour.Blue])}
-          isGameOver={true} />,
-      );
-
+      const wrapper = wrapColourSelect(isGameOver);
       const allDisabled = wrapper.find("button").every("[disabled=true]");
-      expect(allDisabled).to.equal(true);
-    });
-
-    it("is false if game is not over", function() {
-
-      const wrapper = shallow(
-        <ColourSelect
-          onColourSelected={(c: Colour) => { return; }}
-          availableColours={List([Colour.Blue])}
-          isGameOver={false} />,
-      );
-
-      const allEnabled = wrapper.find("button").every("[disabled=false]");
-      expect(allEnabled).to.equal(true);
+      expect(allDisabled).to.equal(isGameOver);
     });
   });
 
   describe("hover halo", function() {
     given(true, false).it("is disabled if game is over", function(isGameOver: boolean) {
 
-      const wrapper = shallow(
-        <ColourSelect
-          onColourSelected={(c: Colour) => { return; }}
-          availableColours={List([Colour.Blue, Colour.Red])}
-          isGameOver={isGameOver} />,
-      );
+      const wrapper = wrapColourSelect(isGameOver);
 
       const allDisabled = wrapper
         .find("button")
