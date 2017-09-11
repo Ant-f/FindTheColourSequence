ed7ca24d6510368360008f530b3c0f6ee224986b
Anthony Fung <Ant-f@users.noreply.github.com>
2017-09-11 23:46:38 +0100

    Sorting colours on colour-select

--- src/containers/colour-select-container.ts
+++ src/containers/colour-select-container.ts
@@ -3,12 +3,18 @@ import { connect, Dispatch } from "react-redux";
 import * as actions from "../action/action-creators";
 import ColourSelect from "../components/colour-select";
 import AppState from "../model/app-state";
-import { Colour } from "../model/colour";
+import { Colour, colourOrder } from "../model/colour";
 import { IDispatchProps, IStateProps } from "../props/colour-select-props";
+
+const compareColour = (c1: Colour, c2: Colour): number => {
+  const value = colourOrder[c1] - colourOrder[c2];
+  return value;
+};
 
 const mapStateToProps = (state: AppState): IStateProps => {
   const colours = Map<Colour, string>(Colour)
     .valueSeq()
+    .sort(compareColour)
     .toList() as List<Colour>;
 
   return {
