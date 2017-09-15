55c023e8770a96762a36dc73d1b62965dd1e278d
Anthony Fung <Ant-f@users.noreply.github.com>
2017-09-15 21:14:56 +0100

    Using how-to-play-item instead of <div/>

--- src/components/how-to-play/Introduction.tsx
+++ src/components/how-to-play/Introduction.tsx
@@ -1,13 +1,10 @@
 import * as React from "react";
-import * as styles from "../../../stylesheets/sass/how-to-play.scss";
+import HowToPlayItem from "./how-to-play-item";
 
 export default class extends React.PureComponent {
   public render() {
     return (
-      <div className={styles.expand}>
-        <h2>
-          Introduction
-        </h2>
+      <HowToPlayItem heading="Introduction">
 
         {/* tslint:disable:max-line-length */}
 
@@ -24,7 +21,8 @@ export default class extends React.PureComponent {
         </p>
 
         {/* tslint:enable:max-line-length */}
-      </div>
+
+      </HowToPlayItem>
     );
   }
 }
