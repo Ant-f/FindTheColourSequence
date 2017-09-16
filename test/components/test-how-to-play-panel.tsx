821db890a682a33e85374cfc89cf4d134ea5bd73
Anthony Fung <Ant-f@users.noreply.github.com>
2017-09-16 23:57:07 +0100

    Adding more tests

--- test/components/test-how-to-play-panel.tsx
+++ test/components/test-how-to-play-panel.tsx
@@ -48,6 +48,41 @@ describe("<HowToPlayPanel/>", function() {
 
       expect(pageDisplay.text()).to.contain(`2/${instance.maxHelpPageCount}`);
     });
+
+    it("doesn't increments displayed page number past maximum", function() {
+
+      // Arrange
+
+      const wrapper = mount(
+        <HowToPlayPanelWithInjection onExitPanel={() => { return; }} />,
+      );
+
+      const initialState: IHowToPlayPanelState = {
+        currentPage: 1,
+      };
+
+      wrapper.setState(initialState);
+
+      const nextButton = wrapper
+        .find("button")
+        .findWhere((w: ReactWrapper) => w.text().indexOf("Next") > -1);
+
+      const instance = wrapper.instance() as HowToPlayPanel;
+
+      // Act
+
+      for (let i = 0; i < instance.maxHelpPageCount; i++) {
+        nextButton.simulate("click");
+      }
+
+      // Assert
+
+      const pageDisplay = wrapper
+        .findWhere((w: ReactWrapper) => w.hasClass(styles.pageNumber));
+
+      const expected = `${instance.maxHelpPageCount}/${instance.maxHelpPageCount}`;
+      expect(pageDisplay.text()).to.contain(expected);
+    });
   });
 
   describe("Prev page button", function() {
@@ -61,6 +96,38 @@ describe("<HowToPlayPanel/>", function() {
 
       const initialState: IHowToPlayPanelState = {
         currentPage: 2,
+      };
+
+      wrapper.setState(initialState);
+
+      const nextButton = wrapper
+        .find("button")
+        .findWhere((w: ReactWrapper) => w.text().indexOf("Prev") > -1);
+
+      // Act
+
+      nextButton.simulate("click");
+
+      // Assert
+
+      const instance = wrapper.instance() as HowToPlayPanel;
+
+      const pageDisplay = wrapper
+        .findWhere((w: ReactWrapper) => w.hasClass(styles.pageNumber));
+
+      expect(pageDisplay.text()).to.contain(`1/${instance.maxHelpPageCount}`);
+    });
+
+    it("doesn't decrement displayed page number to zero", function() {
+
+      // Arrange
+
+      const wrapper = mount(
+        <HowToPlayPanelWithInjection onExitPanel={() => { return; }} />,
+      );
+
+      const initialState: IHowToPlayPanelState = {
+        currentPage: 1,
       };
 
       wrapper.setState(initialState);
